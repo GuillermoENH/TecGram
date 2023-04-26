f98e80f724f041f38eb0a84a25ad41688c0ea381
GuillermoENH <89560933+GuillermoENH@users.noreply.github.com>
2023-04-25 19:02:28 -0600

    Delete framencr directory

--- framencr/config.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	define("s","localhost");
-	define("u","root");	
-	define("p","");
-	define("bd","examente");
- ?>
--- framencr/lib/eventoslib.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	class eventoslib
-	{
-		function Alert($message, $type)
-		{
-		echo '<div class="alert alert-' . $type . '" role="alert">' . $message . '</div>';
-		}
-	}
- ?>
